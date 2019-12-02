2e07f99d3070852e222c673fc218736dd590e871
tmtbe <896369042@qq.com>
2019-12-02 13:33:41 +0800

    feat smartBoy

--- src/main/java/SmartBoy.java
+++ src/main/java/SmartBoy.java
@@ -3,9 +3,10 @@ public class SmartBoy extends GraduateBoy{
     public SmartBoy(String name) {
         super(name);
     }
+
     @Override
-    public void bindParkLot(ParkingLot... parkingLot) {
-        super.bindParkLot(parkingLot);
-        parkingLotList.sort((A,B)-> Long.compare(B.getEmpytyCount(), A.getEmpytyCount()));;
+    public ParkingTicket park(Car car) throws ParkingException {
+        parkingLotList.sort((A,B)-> Long.compare(B.getEmpytyCount(), A.getEmpytyCount()));
+        return super.park(car);
     }
 }
