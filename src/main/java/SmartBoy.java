8d7789b0f6f7efd801c7135eb95ced81df81ae90
tmtbe <896369042@qq.com>
2019-12-02 12:35:56 +0800

    feat smartBoy

--- src/main/java/SmartBoy.java
+++ src/main/java/SmartBoy.java
@@ -8,12 +8,4 @@ public class SmartBoy extends GraduateBoy{
         super.bindParkLot(parkingLot);
         parkingLotList.sort((A,B)-> Long.compare(B.getEmpytyCount(), A.getEmpytyCount()));;
     }
-
-    public static String getName(ParkingTicket parkingTicket) throws TicketException {
-        if (parkingTicket.getMessages().size() >= 2) {
-            return parkingTicket.getMessages().get(1);
-        } else {
-            throw new TicketException("获取信息失败");
-        }
-    }
 }
