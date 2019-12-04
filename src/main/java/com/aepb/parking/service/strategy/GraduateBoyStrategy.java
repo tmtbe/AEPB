0f1e0bd41754756d1dace8fe6a2b0fb7396e9e57
tmtbe <896369042@qq.com>
2019-12-04 12:02:33 +0800

    feat smartBoy

--- src/main/java/com/aepb/parking/service/strategy/GraduateBoyStrategy.java
+++ src/main/java/com/aepb/parking/service/strategy/GraduateBoyStrategy.java
@@ -6,19 +6,13 @@ import com.aepb.parking.exception.ParkingException;
 import com.aepb.parking.service.ParkingBoyStrategy;
 
 import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class GraduateBoyStrategy implements ParkingBoyStrategy {
     @Override
     public ParkingLotEntity getParkingLotEntityFromProvider(ParkingLotProvider parkingLotProvider) throws ParkingException {
-        List<ParkingLotEntity> collect = parkingLotProvider.getParkingLotEntities()
+        return parkingLotProvider.getParkingLotEntities()
                 .stream().filter(n -> getSurplus(n) > 0)
-                .sorted(Comparator.comparing(n -> n.getParkingLot().getId())).collect(Collectors.toList());
-        if (collect.isEmpty()) {
-            throw new ParkingException("没有合适的停车场");
-        } else {
-            return collect.get(0);
-        }
+                .min(Comparator.comparing(n -> n.getParkingLot().getId()))
+                .orElseThrow(() -> new ParkingException("没有合适的停车场"));
     }
 }
--- src/main/java/com/aepb/parking/service/strategy/SmartBoyStrategy.java
+++ src/main/java/com/aepb/parking/service/strategy/SmartBoyStrategy.java
@@ -5,21 +5,18 @@ import com.aepb.parking.entity.ParkingLotProvider;
 import com.aepb.parking.exception.ParkingException;
 import com.aepb.parking.service.ParkingBoyStrategy;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class SmartBoyStrategy implements ParkingBoyStrategy {
 
 
     @Override
     public ParkingLotEntity getParkingLotEntityFromProvider(ParkingLotProvider parkingLotProvider) throws ParkingException {
-        List<ParkingLotEntity> collect = parkingLotProvider.getParkingLotEntities()
-                .stream().filter(n -> getSurplus(n) > 0).sorted((A, B) -> {
-            int result = getSurplus(B).compareTo(getSurplus(A));
-            if (result == 0) result = A.getParkingLot().getId().compareTo(B.getParkingLot().getId());
-            return result;
-                }).collect(Collectors.toList());
-        if (collect.isEmpty()) throw new ParkingException("没有合适的停车场");
-        return collect.get(0);
+        return parkingLotProvider.getParkingLotEntities()
+                .stream().filter(n -> getSurplus(n) > 0)
+                .min((A, B) -> {
+                    int result = getSurplus(B).compareTo(getSurplus(A));
+                    if (result == 0) result = A.getParkingLot().getId().compareTo(B.getParkingLot().getId());
+                    return result;
+                })
+                .orElseThrow(() -> new ParkingException("没有合适的停车场"));
     }
 }
